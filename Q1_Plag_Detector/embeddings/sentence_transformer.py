4ef2b3c5492ef60296235711c9b59e825df59f4c
Aditya Sebastian <aditya26824@gmail.com>
2025-07-05 11:16:53 +0530

    Delete Q1_Plag_Detector directory

--- Q1_Plag_Detector/embeddings/sentence_transformer.py
+++
@@ -1,8 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-class STEmbedder:
-    def __init__(self, model_name='all-MiniLM-L6-v2'):
-        self.model = SentenceTransformer(model_name)
-    
-    def get_embeddings(self, texts):
-        return self.model.encode(texts, convert_to_tensor=True)
--- Q1_Plag_Detector/utils/preprocessing.py
+++
@@ -1,6 +0,0 @@
-import re
-
-def preprocess(text):
-    text = text.lower()
-    text = re.sub(r'\s+', ' ', text.strip())
-    return text
